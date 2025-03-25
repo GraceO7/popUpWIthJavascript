4b3fbe4f45cd058887fc7b3c352857a4e1df8fd3
Ojeyemi Opeyemi <161251930+GraceO7@users.noreply.github.com>
2025-03-25 17:38:58 +0100

    Add files via upload

---
+++ .github/workflows/apps.js
@@ -0,0 +1,10 @@
+
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active')
+   
+    var popup = document.querySelector('.popup');
+    popup.classList.toggle('active')
+
+
+}
